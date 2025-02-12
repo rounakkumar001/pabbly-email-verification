5cbcb343086b64c686622cc291a17aa102218d81
rounakkumar001 <rounakkumar1034@gmail.com>
2025-02-12 17:23:05 +0530

    remove unused variable

--- client/src/sections/dashboard/component/chart/chart-alert.jsx
+++ client/src/sections/dashboard/component/chart/chart-alert.jsx
@@ -5,7 +5,7 @@ import { Box, Alert, Button, Typography } from '@mui/material';
 import axiosInstance, { endpoints } from 'src/utils/axios';
 
 import { fetchEmailVerificationResults } from 'src/redux/slice/email-verification-slice';
-import { resetUpload, completeVerification, startEmailVerification } from 'src/redux/slice/upload-slice';
+import { completeVerification, startEmailVerification } from 'src/redux/slice/upload-slice';
 
 export default function ChartAlert() {
   const dispatch = useDispatch();
